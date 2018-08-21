837707e4d475a670f2e0b7d71867a3e3c323e5b4
Dale Cieslak <dsizzle@users.noreply.github.com>
2018-08-20 23:46:46 -0700

    use setColor instead of recreating pens and brushes

--- model/nibs.py
+++ model/nibs.py
@@ -33,40 +33,40 @@ class Nib(object):
         self.__width = nib.width
         self.__angle = nib.angle
         self.__angle_rads = math.radians(self.__angle)
-        self.__color = nib.get_color()
+        self.__color = nib.color
 
         self.__nibwidth_x = self.width * math.cos(self.__angle_rads)
         self.__nibwidth_y = self.width * math.sin(self.__angle_rads)
 
         self.seed = time.localtime()
-        self.pen = QtGui.QPen(QtGui.QColor(self.__color.red(), self.__color.green(), \
-            self.__color.blue(), 90), 2, QtCore.Qt.SolidLine)
-        self.brush = QtGui.QBrush(QtGui.QColor(self.__color.red(), self.__color.green(), \
-            self.__color.blue(), 220), QtCore.Qt.SolidPattern)
+        self.pen.setColor(QtGui.QColor(self.__color.red(), self.__color.green(), \
+            self.__color.blue(), 90))
+        self.brush.setColor(QtGui.QColor(self.__color.red(), self.__color.green(), \
+            self.__color.blue(), 220))
 
     def get_color(self):
         return self.__color
 
     def set_color(self, color):
         self.__color = color
-        self.pen = QtGui.QPen(QtGui.QColor(self.__color.red(), self.__color.green(), \
-            self.__color.blue(), 90), 2, QtCore.Qt.SolidLine)
-        self.brush = QtGui.QBrush(QtGui.QColor(self.__color.red(), self.__color.green(), \
-            self.__color.blue(), 220), QtCore.Qt.SolidPattern)
+        self.pen.setColor(QtGui.QColor(self.__color.red(), self.__color.green(), \
+            self.__color.blue(), 90))
+        self.brush.setColor(QtGui.QColor(self.__color.red(), self.__color.green(), \
+            self.__color.blue(), 220))
 
     color = property(get_color, set_color)
 
     def set_alpha(self, alpha):
-        self.pen = QtGui.QPen(QtGui.QColor(self.__color.red(), self.__color.green(), \
-            self.__color.blue(), ((90.0 / 255.0) * alpha)), 2, QtCore.Qt.SolidLine)
-        self.brush = QtGui.QBrush(QtGui.QColor(self.__color.red(), self.__color.green(), \
-            self.__color.blue(), ((220.0 / 255.0) * alpha)), QtCore.Qt.SolidPattern)
+        self.pen.setColor(QtGui.QColor(self.__color.red(), self.__color.green(), \
+            self.__color.blue(), ((90.0 / 255.0) * alpha)))
+        self.brush.setColor(QtGui.QColor(self.__color.red(), self.__color.green(), \
+            self.__color.blue(), ((220.0 / 255.0) * alpha)))
 
     def reset_alpha(self):
-        self.pen = QtGui.QPen(QtGui.QColor(self.__color.red(), self.__color.green(), \
-            self.__color.blue(), 90), 2, QtCore.Qt.SolidLine)
-        self.brush = QtGui.QBrush(QtGui.QColor(self.__color.red(), self.__color.green(), \
-            self.__color.blue(), 220), QtCore.Qt.SolidPattern)
+        self.pen.setColor(QtGui.QColor(self.__color.red(), self.__color.green(), \
+            self.__color.blue(), 90))
+        self.brush.setColor(QtGui.QColor(self.__color.red(), self.__color.green(), \
+            self.__color.blue(), 220))
 
     def set_angle(self, angle):
         if angle < 0:
