5ea575320784ddef143e4f8bb96c2a8eb5e63f1b
Dale Cieslak <dsizzle@users.noreply.github.com>
2018-10-14 11:12:29 -0700

    rebuild character xref on load

--- model/character_set.py
+++ model/character_set.py
@@ -465,3 +465,7 @@ class CharacterSet(object):
                 print "ERROR: Problem reading file: ", err
                 return
 
+        for char_id in self.__objects[CHAR_TYPE].keys():
+            char_item = self.get_item_by_index(char_id)
+            if char_item.unicode_character >= 0:
+                self.__character_xref[unichr(char_item.unicode_character)] = char_id
